e4fd2db1b1e7fdcaac039e364105d4ce85bd5c73
R0CKSTAR <yeahdongcn@gmail.com>
2014-06-13 13:52:37 +0800

    Add extension

---
+++ UIColorExtension.swift
@@ -0,0 +1,43 @@
+//
+//  UIColorExtension.swift
+//  RSBarcodesSample
+//
+//  Created by R0CKSTAR on 6/13/14.
+//  Copyright (c) 2014 P.D.Q. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(rgba: String) {
+        var red: CGFloat   = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat  = 0.0
+        var alpha: CGFloat = 1.0
+        
+        if rgba.hasPrefix("#") {
+            let hex = rgba.substringFromIndex(1)
+            let scanner = NSScanner.scannerWithString(hex)
+            var hexValue: CUnsignedLongLong = 0
+            if scanner.scanHexLongLong(&hexValue) {
+                if hex.length() == 6 {
+                    red   = CGFloat((hexValue & 0xFF0000) >> 16) / 255.0
+                    green = CGFloat((hexValue & 0x00FF00) >> 8)  / 255.0
+                    blue  = CGFloat(hexValue & 0x0000FF) / 255.0
+                } else if hex.length() == 8 {
+                    red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                    green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                    blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
+                    alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
+                } else {
+                    print("invalid rgb string, length should be 7 or 9")
+                }
+            } else {
+                println("scan hex error")
+            }
+        } else {
+            print("invalid rgb string, missing '#' as prefix")
+        }
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+}
