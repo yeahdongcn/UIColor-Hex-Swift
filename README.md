75ba3ba6a863f95ac54f0086d0883152749bf515
Yichi Zhang <zhang-yi-chi@hotmail.com>
2014-12-12 03:32:37 +1100

    Added support for 3-digit and 4-digit notation

--- README.md
+++ README.md
@@ -9,5 +9,7 @@ Convenience method for creating autoreleased color using RGBA hex string.
     var strokeColor = UIColor(rgba: "#ffcc00").CGColor // Solid color
     
     var fillColor = UIColor(rgba: "#ffcc00dd").CGColor // Color with alpha
+
+    var backgroundColor = UIColor(rgba: "#FFF").CGColor // Supports shorthand 3 digit representation
     
 See more in [RSBarcodes_Swift](https://github.com/yeahdongcn/RSBarcodes_Swift) and [objc version](https://github.com/yeahdongcn/RSBarcodes) 
--- UIColorExtension.swift
+++ UIColorExtension.swift
@@ -21,23 +21,32 @@ extension UIColor {
             let scanner = NSScanner(string: hex)
             var hexValue: CUnsignedLongLong = 0
             if scanner.scanHexLongLong(&hexValue) {
-                if countElements(hex) == 6 {
+                if countElements(hex) == 3 {
+                    red   = CGFloat((hexValue & 0xF00) >> 8)  / 15.0
+                    green = CGFloat((hexValue & 0x0F0) >> 4)  / 15.0
+                    blue  = CGFloat(hexValue & 0x00F)         / 15.0
+                } else if countElements(hex) == 4 {
+                    red   = CGFloat((hexValue & 0xF000) >> 12) / 15.0
+                    green = CGFloat((hexValue & 0x0F00) >> 8)  / 15.0
+                    blue  = CGFloat((hexValue & 0x00F0) >> 4)  / 15.0
+                    alpha = CGFloat(hexValue & 0x000F)         / 15.0
+                } else if countElements(hex) == 6 {
                     red   = CGFloat((hexValue & 0xFF0000) >> 16) / 255.0
                     green = CGFloat((hexValue & 0x00FF00) >> 8)  / 255.0
-                    blue  = CGFloat(hexValue & 0x0000FF) / 255.0
+                    blue  = CGFloat(hexValue & 0x0000FF)         / 255.0
                 } else if countElements(hex) == 8 {
                     red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
                     green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
                     blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
                     alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
                 } else {
-                    print("invalid rgb string, length should be 7 or 9")
+                    print("Invalid RGB string, number of characters after '#'' should be either 3, 4, 6 or 8")
                 }
             } else {
-                println("scan hex error")
+                println("Scan hex error")
             }
         } else {
-            print("invalid rgb string, missing '#' as prefix")
+            print("Invalid RGB string, missing '#' as prefix")
         }
         self.init(red:red, green:green, blue:blue, alpha:alpha)
     }
