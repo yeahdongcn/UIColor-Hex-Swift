50b3a6e2754c4b0026830c69015ea252c10b0f4d
R0CKSTAR <yeahdongcn@gmail.com>
2014-12-12 10:19:20 +0800

    Use switch case instead of if-else

--- UIColorExtension.swift
+++ UIColorExtension.swift
@@ -21,26 +21,32 @@ extension UIColor {
             let scanner = NSScanner(string: hex)
             var hexValue: CUnsignedLongLong = 0
             if scanner.scanHexLongLong(&hexValue) {
-                if countElements(hex) == 3 {
-                    red   = CGFloat((hexValue & 0xF00) >> 8)  / 15.0
-                    green = CGFloat((hexValue & 0x0F0) >> 4)  / 15.0
-                    blue  = CGFloat(hexValue & 0x00F)         / 15.0
-                } else if countElements(hex) == 4 {
-                    red   = CGFloat((hexValue & 0xF000) >> 12) / 15.0
-                    green = CGFloat((hexValue & 0x0F00) >> 8)  / 15.0
-                    blue  = CGFloat((hexValue & 0x00F0) >> 4)  / 15.0
-                    alpha = CGFloat(hexValue & 0x000F)         / 15.0
-                } else if countElements(hex) == 6 {
-                    red   = CGFloat((hexValue & 0xFF0000) >> 16) / 255.0
-                    green = CGFloat((hexValue & 0x00FF00) >> 8)  / 255.0
-                    blue  = CGFloat(hexValue & 0x0000FF)         / 255.0
-                } else if countElements(hex) == 8 {
+                switch (countElements(hex)) {
+                case 3:
+                    red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
+                    green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
+                    blue  = CGFloat(hexValue & 0x00F)              / 15.0
+                    break
+                case 4:
+                    red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
+                    green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
+                    blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
+                    alpha = CGFloat(hexValue & 0x000F)             / 15.0
+                    break
+                case 6:
+                    red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
+                    green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
+                    blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
+                    break
+                case 8:
                     red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
                     green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
                     blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
                     alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
-                } else {
+                    break
+                default:
                     print("Invalid RGB string, number of characters after '#' should be either 3, 4, 6 or 8")
+                    break
                 }
             } else {
                 println("Scan hex error")
