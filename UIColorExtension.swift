cbea55accbe85fe86d107cca390411ac16632b12
R0CKSTAR <yeahdongcn@gmail.com>
2015-01-04 09:27:07 +0800

    Swift switch-case do not need break

--- UIColorExtension.swift
+++ UIColorExtension.swift
@@ -26,27 +26,22 @@ extension UIColor {
                     red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
                     green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
                     blue  = CGFloat(hexValue & 0x00F)              / 15.0
-                    break
                 case 4:
                     red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
                     green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
                     blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
                     alpha = CGFloat(hexValue & 0x000F)             / 15.0
-                    break
                 case 6:
                     red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
                     green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
                     blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
-                    break
                 case 8:
                     red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
                     green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
                     blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
                     alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
-                    break
                 default:
                     print("Invalid RGB string, number of characters after '#' should be either 3, 4, 6 or 8")
-                    break
                 }
             } else {
                 println("Scan hex error")
