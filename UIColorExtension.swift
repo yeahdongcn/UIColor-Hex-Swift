c087d53f546a6ffc523e628413f54c95739f615a
Kilian Koeltzsch <me@kilian.io>
2015-03-19 10:38:24 +0100

    countElements has been renamed to count

--- UIColorExtension.swift
+++ UIColorExtension.swift
@@ -21,7 +21,7 @@ extension UIColor {
             let scanner = NSScanner(string: hex)
             var hexValue: CUnsignedLongLong = 0
             if scanner.scanHexLongLong(&hexValue) {
-                switch (countElements(hex)) {
+                switch (count(hex)) {
                 case 3:
                     red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
                     green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
