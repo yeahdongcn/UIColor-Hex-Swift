242cdbe27807cde5229c0249284c03182e673ffc
Yuki Nagai <ynagai@r.recruit.co.jp>
2015-10-14 17:44:57 +0900

    Testing

--- HEXColorTests/HEXColorTests.swift
+++ HEXColorTests/HEXColorTests.swift
@@ -23,7 +23,10 @@ class HEXColorTests: XCTestCase {
     
     /*
     Test cases
-    * Minimum: 0x000000
+    * Minimum: 0x00000000
+    * Step:    0x12345678
+      * Alpha:   0x123456, alpha = 0.5 (3-digit/6-digit)
+    * Maximum: 0xFFFFFFFF
     */
     
     // MARK: - Three-digit
@@ -35,11 +38,94 @@ class HEXColorTests: XCTestCase {
         XCTAssertEqual(alpha, 0xFF)
     }
     
-    func testThreeDigitMin() {
-        let (red, green, blue, alpha) = UIColor(hex3: 0x000).rgba()
+    func testThreeDigitStep() {
+        let (red, green, blue, alpha) = UIColor(hex3: 0x123, alpha: 0.5).rgba()
+        XCTAssertEqual(red, 0x11)
+        XCTAssertEqual(green, 0x22)
+        XCTAssertEqual(blue, 0x33)
+        XCTAssertEqual(alpha, 0xFF / 2)
+    }
+    
+    func testThreeDigitMax() {
+        let (red, green, blue, alpha) = UIColor(hex3: 0xFFF).rgba()
+        XCTAssertEqual(red, 0xFF)
+        XCTAssertEqual(green, 0xFF)
+        XCTAssertEqual(blue, 0xFF)
+        XCTAssertEqual(alpha, 0xFF)
+    }
+    
+    // MARK: - Four-digit
+    func testFourDigitMin() {
+        let (red, green, blue, alpha) = UIColor(hex4: 0x0000).rgba()
         XCTAssertEqual(red, 0)
         XCTAssertEqual(green, 0)
         XCTAssertEqual(blue, 0)
+        XCTAssertEqual(alpha, 0)
+    }
+    
+    func testFourDigitStep() {
+        let (red, green, blue, alpha) = UIColor(hex4: 0x1234).rgba()
+        XCTAssertEqual(red, 0x11)
+        XCTAssertEqual(green, 0x22)
+        XCTAssertEqual(blue, 0x33)
+        XCTAssertEqual(alpha, 0x44)
+    }
+    
+    func testFourDigitMax() {
+        let (red, green, blue, alpha) = UIColor(hex4: 0xFFFF).rgba()
+        XCTAssertEqual(red, 0xFF)
+        XCTAssertEqual(green, 0xFF)
+        XCTAssertEqual(blue, 0xFF)
+        XCTAssertEqual(alpha, 0xFF)
+    }
+    
+    // MARK: - Six-digit
+    func testSixDigitMin() {
+        let (red, green, blue, alpha) = UIColor(hex6: 0x000000).rgba()
+        XCTAssertEqual(red, 0)
+        XCTAssertEqual(green, 0)
+        XCTAssertEqual(blue, 0)
+        XCTAssertEqual(alpha, 0xFF)
+    }
+    
+    func testSixDigitStep() {
+        let (red, green, blue, alpha) = UIColor(hex6: 0x123456, alpha: 0.5).rgba()
+        XCTAssertEqual(red, 0x12)
+        XCTAssertEqual(green, 0x34)
+        XCTAssertEqual(blue, 0x56)
+        XCTAssertEqual(alpha, 0xFF / 2)
+    }
+    
+    func testSixDigitMax() {
+        let (red, green, blue, alpha) = UIColor(hex6: 0xFFFFFF).rgba()
+        XCTAssertEqual(red, 0xFF)
+        XCTAssertEqual(green, 0xFF)
+        XCTAssertEqual(blue, 0xFF)
+        XCTAssertEqual(alpha, 0xFF)
+    }
+    
+    // MARK: - Eight-digit
+    func testEightDigitMin() {
+        let (red, green, blue, alpha) = UIColor(hex8: 0x00000000).rgba()
+        XCTAssertEqual(red, 0)
+        XCTAssertEqual(green, 0)
+        XCTAssertEqual(blue, 0)
+        XCTAssertEqual(alpha, 0)
+    }
+    
+    func testEightDigitStep() {
+        let (red, green, blue, alpha) = UIColor(hex8: 0x12345678).rgba()
+        XCTAssertEqual(red, 0x12)
+        XCTAssertEqual(green, 0x34)
+        XCTAssertEqual(blue, 0x56)
+        XCTAssertEqual(alpha, 0x78)
+    }
+    
+    func testEightDigitMax() {
+        let (red, green, blue, alpha) = UIColor(hex6: 0xFFFFFFFF).rgba()
+        XCTAssertEqual(red, 0xFF)
+        XCTAssertEqual(green, 0xFF)
+        XCTAssertEqual(blue, 0xFF)
         XCTAssertEqual(alpha, 0xFF)
     }
 }
