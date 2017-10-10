b6bc0adba02efcecd285680c184780cb04ec8b0b
John Codeos <JEKos@users.noreply.github.com>
2017-10-11 01:24:49 +0300

    characters are deprecated in Swift 4
    Characters are deprecated in Swift 4, Fix xcode 9 warnings

--- HEXColor/UIColorExtension.swift
+++ HEXColor/UIColorExtension.swift
@@ -95,7 +95,7 @@ extension UIColor {
             throw UIColorInputError.unableToScanHexValue
         }
         
-        switch (hexString.characters.count) {
+        switch (hexString.count) {
         case 3:
             self.init(hex3: UInt16(hexValue))
         case 4:
@@ -167,16 +167,16 @@ extension String {
             return nil
         }
         
-        let hexString: String = self.substring(from: self.characters.index(self.startIndex, offsetBy: 1))
-        switch (hexString.characters.count) {
+        let hexString: String = self.substring(from: self.index(self.startIndex, offsetBy: 1))
+        switch (hexString.count) {
         case 4:
             return "#"
-                + hexString.substring(from: self.characters.index(self.startIndex, offsetBy: 1))
-                + hexString.substring(to: self.characters.index(self.startIndex, offsetBy: 1))
+                + hexString.substring(from: self.index(self.startIndex, offsetBy: 1))
+                + hexString.substring(to: self.index(self.startIndex, offsetBy: 1))
         case 8:
             return "#"
-                + hexString.substring(from: self.characters.index(self.startIndex, offsetBy: 2))
-                + hexString.substring(to: self.characters.index(self.startIndex, offsetBy: 2))
+                + hexString.substring(from: self.index(self.startIndex, offsetBy: 2))
+                + hexString.substring(to: self.index(self.startIndex, offsetBy: 2))
         default:
             return nil
         }
