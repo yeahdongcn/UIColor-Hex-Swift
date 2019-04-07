991f274ea711b9ed4e7b6f52120fef78e08e4add
YanMeow <liyan_helloworld@163.com>
2019-04-07 12:43:44 +0800

    Update ci.sh

--- Scripts/ci.sh
+++ Scripts/ci.sh
@@ -2,6 +2,8 @@
 
 set -e
 
-xcodebuild -project HEXColor.xcodeproj -scheme "HEXColor-iOS" -destination "platform=iOS Simulator,name=iPhone 6" test
+xcodebuild -project HEXColor.xcodeproj -scheme "HEXColor-iOS" -destination "platform=iOS Simulator,name=iPhone 6" clean test
 
-xcodebuild -project HEXColor.xcodeproj -scheme "HEXColor-watchOS" -destination "platform=watchOS Simulator,name=Apple Watch - 38mm" build
+xcodebuild -project HEXColor.xcodeproj -scheme "HEXColor-watchOS" -destination "platform=watchOS Simulator,name=Apple Watch - 38mm" clean build
+
+xcodebuild -project HEXColor.xcodeproj -scheme "HEXColor-macOS" -destination "platform=OS X,arch=x86_64" clean build
