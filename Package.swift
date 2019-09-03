4ffdae17a9cf7503d2313aad03386ee17d4e23d9
Zhou Hao <hao.zhou@cognizant.com>
2019-09-03 18:05:51 +0800

    5.1.2. Fixed Package.swift error

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HEXColor",
-            dependencies: []),
+            dependencies: [],
             path: "HEXColor"
+        )
     ]
 )
