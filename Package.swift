d811770aa5f4f8bf0eb3407c74318a6558b37d03
Zhou Hao <hao.zhou@cognizant.com>
2019-09-03 17:53:49 +0800

    Change target name

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "UIColor-Hex-Swift",
-            targets: ["UIColor-Hex-Swift"]),
+            targets: ["HEXColor"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,12 +19,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "UIColor-Hex-Swift",
+            name: "HEXColor",
             dependencies: []),
-            path: "HEXColor"
-        .testTarget(
-            name: "UIColor-Hex-SwiftTests",
-            dependencies: ["UIColor-Hex-Swift"]),
             path: "HEXColor"
     ]
 )
