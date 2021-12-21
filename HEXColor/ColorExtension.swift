b2529f01eb8ec3194bb9925827ca9a54dd7f5687
Mikhail Barashkov <git@mbarashkov.email>
2021-12-21 14:23:12 +0300

    Add a color extension for SwiftUI

---
+++ HEXColor/ColorExtension.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+@available(watchOSApplicationExtension 6.0, *)
+@available(macOS 10.15, *)
+@available(iOSApplicationExtension 13.0, *)
+extension Color {
+    public init(hex3: UInt16, alpha: CGFloat = 1) {
+        self.init(PlatformColor(hex3: hex3, alpha: alpha))
+    }
+
+    public init(hex4: UInt16) {
+        self.init(PlatformColor(hex4: hex4))
+    }
+
+    public init(hex6: UInt32, alpha: CGFloat = 1) {
+        self.init(PlatformColor(hex6: hex6, alpha: alpha))
+    }
+
+    public init(hex8: UInt32) {
+        self.init(PlatformColor(hex8: hex8))
+    }
+
+#if os(macOS)
+    public init?(_ rgba: String, defaultColor: NSColor = NSColor.clear) {
+        if let platformColor = PlatformColor(rgba, defaultColor: defaultColor) {
+            self.init(platformColor)
+        } else {
+            return nil
+        }
+    }
+#else
+    public init(_ rgba: String, defaultColor: UIColor = UIColor.clear) {
+        self.init(PlatformColor(rgba, defaultColor: defaultColor))
+    }
+#endif
+}
